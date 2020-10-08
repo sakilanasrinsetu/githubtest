227e2fa8545c8ce70238ad6fba01d09bde1faee6
sakilanasrinsetu <71583839+sakilanasrinsetu@users.noreply.github.com>
2020-10-08 11:41:44 +0600

    Update game.py

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ display=pygame.display.set_mode((1000,600))		#set width & height of display
 pygame.display.set_caption("Car Game")		#set window name
 carimg=pygame.image.load("car1.png")	#load car image
 backgroundleft=pygame.image.load("left.png")	#load background left side image
-backgroundright=pygame.image.load("right.png")	#load background right side image 
+backgroundright=pygame.image.load("right.png")	#load background right side image
 car_width=23
 import time 	#import the time for restart the game
 import random 	#import for random position police car comes
@@ -22,18 +22,21 @@ def policecar(police_startx,police_starty,police): 	#define police function
 
 	display.blit(police_come,(police_startx,police_starty))	#display the police car
 
+
 def crash():		#create crash function to display this message
 	message_display("Car Crashed")	#display this message call the message_display function
-	
+
 def message_display(text):		#create function for message edit
 	largetext=pygame.font.Font("freesansbold.ttf",80)	#message in this style and the size will be 80
-	textsurf,textrect=text_object(text,largetext)	#create function to edit message 
+	textsurf,textrect=text_object(text,largetext)	#create function to edit message
 	textrect.center=((400),(300))	#show the message position in display
-	display.blit(textsurf,textrect)	#display this message 
+	display.blit(textsurf,textrect)	#display this message
 	pygame.display.update()	#update display
 	time.sleep(3)		#after crashed 3 sec restart the game
 	loop()		#call the loop function to restart the game
 
+
+#new Line added
 def text_object(text,font):		#display after crash the car
 	textsurface=font.render(text,True,black)	#display in this colour
 	return textsurface,textsurface.get_rect()	#after that restart the game & ready to give some input
@@ -41,7 +44,7 @@ def text_object(text,font):		#display after crash the car
 
 
 def background():
-	display.blit(backgroundleft,(0,0))	#set left side position of background image at x axis & y axis 
+	display.blit(backgroundleft,(0,0))	#set left side position of background image at x axis & y axis
 	display.blit(backgroundright,(700,0))	#set right side position of background image at x axis & y axis
 
 def car(x,y):		#create car function
@@ -81,18 +84,18 @@ def loop():		#all the function are called using this function
 		police_starty-=(policecar_speed/4)		#police car speed at y axis
 		policecar(police_startx,police_starty,police)	#call police function
 		police_starty+=policecar_speed			#police car speed increse
-		car(x,y)	#call the function of car	
+		car(x,y)	#call the function of car
 		if x<130 or x>700-car_width:		#if car goes out of this range
 		#	bumped=True				#stop the game
 			crash()					#call crash function
 		if police_starty>600:		#police car pass it without crashed
 			police_starty=0-police_height	#only one car is crossed
-			police_startx=random.randrange(130,(1000-300))	#anthor car is come 
+			police_startx=random.randrange(130,(1000-300))	#anthor car is come
 			police=random.randrange(0,2)	#diffrent car come
 
 		if y<police_starty+police_height: #if police car not pass
 			if x > police_startx and x < police_startx + police_width or x + car_width > police_startx and x + car_width < police_startx + police_width :
-				crash()	#crash the car			
+				crash()	#crash the car
 
 		pygame.display.update()		#update the display
 loop()		#call the loop function
